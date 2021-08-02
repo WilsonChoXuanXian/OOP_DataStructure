b10e66dbe546e1d6657f07f5b2b5de0fd40f2b0b
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-02 12:10:16 +0800

    Comparison using command prompt

---
+++ Comparison.java
@@ -0,0 +1,23 @@
+public class Comparison {
+	public static void main (String[] args){
+		// primitive
+		int a = 20;
+		int b = 20;
+		if (a < b)
+			System.out.println("a < b");
+		else if (a == b)
+			System.out.println("a = b");
+		else
+			System.out.println("a > b");
+
+		// reference
+		String s1 = "bb";
+		String s2 = "bb";
+		if (s1.compareTo(s2) < 0)
+			System.out.println("s1 < s2");
+		else if (s1.compareTo(s2) == 0)
+			System.out.println("s1 = s2");
+		else
+			System.out.println("s1 < s2");
+	}
+}
