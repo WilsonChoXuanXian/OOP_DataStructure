3383512482298c302d5b0bbe5b45930723fce8fd
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-06 02:13:11 +0800

    another way of initialising arrays

--- Lecture_OOPDS/TestArray.java
+++ Lecture_OOPDS/TestArray.java
@@ -5,6 +5,9 @@ import java.util.Random;
 
 public class TestArray {
   public static void main (String[] args) {
+
+    // int[] mylist = {2, 1, 3, 4, 5};    // this is another way of initialising Arrays
+
     int[] myList = new int[5];
     Random r = new Random();
     for (int i = 0; i < myList.length; i++)
