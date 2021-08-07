e04049c91c537bebba0cae3cde5610fa97b127cd
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-07 17:25:55 +0800

    For-each loop

--- Lecture_OOPDS/TestArray.java
+++ Lecture_OOPDS/TestArray.java
@@ -5,14 +5,18 @@ import java.util.Random;
 
 public class TestArray {
   public static void main (String[] args) {
-
-    // int[] mylist = {2, 1, 3, 4, 5};    // this is another way of initialising Arrays
-
     int[] myList = new int[5];
+    // int[] mylist = {2, 1, 3, 4, 5};                // this is another way of initialising Arrays
     Random r = new Random();
+    
     for (int i = 0; i < myList.length; i++)
       myList[i] = r.nextInt(100);
     System.out.println (Arrays.toString(myList));
+
+    for(int element : myList)                         // "For-each loop" to print out Array element
+      System.out.print(element + " ");
+    System.out.println();
+
     Arrays.sort(myList);
     System.out.println (Arrays.toString(myList));
   }
