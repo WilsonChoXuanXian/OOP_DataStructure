61a6d33b0cbccc93693d8a06b043c8402638c4f7
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-18 21:40:03 +0800

    changes in folder

--- Lab_OOPDS/Lab00_q2.java
+++ Lab_OOPDS/Lab00_q2.java
@@ -1,4 +1,4 @@
-package Lab_OOPDS.Lab00;
+package Lab_OOPDS;
 
 import java.util.Scanner;
 import java.util.ArrayList;
--- Lab_OOPDS/Lab01_q3.java
+++ Lab_OOPDS/Lab01_q3.java
@@ -1,4 +1,4 @@
-package Lab_OOPDS.Lab01;
+package Lab_OOPDS;
 
 import java.util.Scanner;
 import java.util.ArrayList;
