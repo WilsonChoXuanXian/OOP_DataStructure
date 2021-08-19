1408b99e5e04cd0cbd36e026ae62e03584f93ffe
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-19 18:05:26 +0800

    lab02 question2 done

--- Lab_OOPDS/Lab02_q2/Account.java
+++ Lab_OOPDS/Lab02_q2/Account.java
@@ -10,9 +10,9 @@ public class Account {
     }
 
     public Account(int id, double balance) {
+        this.id = id;
         if(balance < 100)
             balance = 100;
-        this.id = id;
         this.balance = balance;
         dateCreated = new java.util.Date();
     }
@@ -40,3 +40,49 @@ public class Account {
         balance += depositAmount;
     }
 }
+
+/*-----------------------------Below are the tutor's answer-------------------------------------
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+
+    private Date dateCreated = new Date();
+
+    public Account() {
+
+    }
+
+    public Account(int id, double balance) {
+        this.id = id;
+        if(balance >= 100)
+            this.balance = balance;
+        else
+            System.out.println("Error: initial balance assigned is insufficient");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public java.util.Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void withdraw(double withdrawAmount) {
+        if (balance > withdrawAmount)
+            balance -= withdrawAmount;
+        else
+            System.out.println("Error: Insufficient balance");
+    }
+
+    public void deposit(double depositAmount) {
+        balance += depositAmount;
+    }
+}
+*/
--- Lab_OOPDS/Lab02_q2/testAccount.java
+++ Lab_OOPDS/Lab02_q2/testAccount.java
@@ -3,18 +3,18 @@ package Lab_OOPDS.Lab02_q2;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class testAccount {
+/* public class testAccount {
     public static void main (String[] args) {
-        ArrayList<Account> accountArrayList = new ArrayList<>();
+        ArrayList<Account> accounts = new ArrayList<>();
 
         Account account1 = new Account(101,1000);
-        accountArrayList.add(account1);
+        accounts.add(account1);
 
         Account account2 = new Account(102,2000);
-        accountArrayList.add(account2);
+        accounts.add(account2);
 
         Account account3 = new Account(103,3000);
-        accountArrayList.add(account3);
+        accounts.add(account3);
 
         System.out.println("Account id = " + account1.getId());
         System.out.println("Created    = " + account1.getDateCreated());
@@ -46,9 +46,50 @@ public class testAccount {
         System.out.println("Balance            = RM" + account1.getBalance());
         System.out.println();
 
-        System.out.print("Enter amount to deposit = RM");
+        System.out.print("Enter amount to withdraw = RM");
         userWithdrawAmount = input.nextDouble();
         account1.withdraw(userWithdrawAmount);
         System.out.println("Balance            = RM" + account1.getBalance());
+    }
+}
+ */
+// ------------------------------Below are the tutor's answer---------------------------------
+
+public class testAccount {
+    public static void main (String[] args) {
+        ArrayList<Account> accounts = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++)
+            accounts.add (new Account(101+i, (i+1)*1000));
+
+        for(int i = 0; i < accounts.size(); i++) {
+            System.out.println ("Account id  = " + accounts.get(i).getId());
+            System.out.println ("Created     = " + accounts.get(i).getDateCreated());
+            System.out.printf ("Balance      = RM%.2f%n", 
+                                accounts.get(i).getBalance());
+            System.out.println ();
+        }
+
+        Scanner input = new Scanner(System.in);
+        Account firstAccount = accounts.get(0);
+        System.out.println("First Account ID = " + firstAccount.getId());
+
+        System.out.print ("Enter amount to deposit = RM");
+        double amount = input.nextDouble();
+        firstAccount.deposit(amount);
+        System.out.printf ("Balance            = RM%.2f%n%n",
+                            firstAccount.getBalance());
+
+        System.out.print ("Enter amount to withdraw = RM");
+        amount = input.nextDouble();
+        firstAccount.withdraw(amount);
+        System.out.printf ("Balance            = RM%.2f%n%n",
+                            firstAccount.getBalance());
+
+        System.out.print("Enter amount to withdraw = RM");
+        amount = input.nextDouble();
+        firstAccount.withdraw(amount);
+        System.out.printf ("Balance            = RM%.2f%n%n",
+                            firstAccount.getBalance());
     }
 }
