14c9d767698b707fecc0a0c48e2b683c128b0158
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-24 23:04:31 +0800

    lab03 question2

---
+++ Lab_OOPDS/Lab03_q1n2/TestTriangle.java
@@ -0,0 +1,5 @@
+package Lab_OOPDS.Lab03_q1n2;
+
+public class TestTriangle {
+    
+}
--- Lab_OOPDS/Lab03_q1n2/Triangle.java
+++ Lab_OOPDS/Lab03_q1n2/Triangle.java
@@ -1,5 +1,35 @@
 package Lab_OOPDS.Lab03_q1n2;
 
 public class Triangle {
-    private 
+    private Point[] points = new Point[3];
+    private double perimeter;
+
+    public Triangle() {
+        for (int i = 0; i < points.length; i++)
+            points[i] = new Point(0,0);
+    }
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        points[0] = p1;
+        points[1] = p2;
+        points[2] = p3;
+        calcPerimeter();
+    }
+
+    private void calcPerimeter() {
+        double distance_p1_p2 = points[0].distance(points[1]);
+        double distance_p2_p3 = points[1].distance(points[2]);
+        double distance_p1_p3 = points[2].distance(points[1]);
+
+        perimeter = distance_p1_p2 + distance_p2_p3 + distance_p1_p3;
+    }
+
+    public String toString() {
+        String string = "Points = " + 
+                            points[0].toString() + " " + 
+                                points[1].toString() + " " + 
+                                        points[2].toString() + 
+                        ". Perimeter = " + perimeter;
+        return string;
+    }
 }
