084a29832c316bd351e31cfabbdcd224840705e2
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-09-06 09:49:42 +0800

    adding "finally"

--- Lecture_OOPDS/CatchException.java
+++ Lecture_OOPDS/CatchException.java
@@ -3,33 +3,42 @@ package Lecture_OOPDS;
 public class CatchException {
     public static void main(String[] args) {
         try {
+            System.out.println("main");
             method1();
-            System.out.println("method1 is called");
         } catch(Exception ex) {
-            System.out.println("main method");
+            System.out.println("main method's catch");
+        } finally {
+            System.out.println("main's finally");
         }
         System.out.println("main method after try catch block");
     }
 
     public static void method1() {
         try {
+            System.out.println("method1");
             method2();
         } catch(ArithmeticException ex) {
-            System.out.println("method1");
+            System.out.println("method1's catch");
+        } finally {
+            System.out.println("method1's finally");
         }
         System.out.println("method1 after try catch block");
     }
 
     public static void method2() {
         try {
+            System.out.println("method2");
             method3();
         } catch(NullPointerException ex) {
-            System.out.println("method2");
+            System.out.println("method2's catch");
+        } finally {
+            System.out.println("method2's finally");
         }
-        System.out.println("see if this line will be printed out");
+        System.out.println("method2 after try catch block");
     }
 
     public static void method3() {
-        throw new ArithmeticException("error");
+        System.out.println("method3");
+        throw new ArithmeticException("Error");
     }
 }
