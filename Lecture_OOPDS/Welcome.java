fbb78db9bdeb7237cd3d8e43670d6828884df168
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-09-09 13:46:38 +0800

    testing

--- Lecture_OOPDS/Welcome.java
+++ Lecture_OOPDS/Welcome.java
@@ -5,6 +5,6 @@ package Lecture_OOPDS;
 
 public class Welcome {
     public static void main (String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World!!");
     }
 }
