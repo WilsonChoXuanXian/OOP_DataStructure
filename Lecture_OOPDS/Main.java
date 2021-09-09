1396166b64633d4a1c6224ff33e60c77f49e7f62
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-09-09 13:50:09 +0800

    parse to String

---
+++ Lecture_OOPDS/Main.java
@@ -0,0 +1,16 @@
+package Lecture_OOPDS;
+
+public class Main {
+    public static void main(String[] args) {
+        String x = "10";
+        short s = Short.parseShort(x);      // String to short
+        System.out.println(s);
+
+        int y = Integer.parseInt(x);        // String to integer
+        System.out.println(y);
+
+        x = "10.5";
+        double z = Double.parseDouble(x);   // String to double
+        System.out.println(z);
+    }
+}
