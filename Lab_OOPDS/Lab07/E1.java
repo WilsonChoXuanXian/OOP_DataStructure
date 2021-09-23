85444105f47de9c69ee305821ec4e1beb1fc10bd
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-09-23 18:03:52 +0800

    lab07

---
+++ Lab_OOPDS/Lab07/E1.java
@@ -0,0 +1,30 @@
+package Lab_OOPDS.Lab07;
+
+import java.util.Scanner;
+
+public class E1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a positive long integer: ");
+        long number = input.nextLong();
+
+        System.out.println("(Interative) The sum of digits in " + number + " is " + sumDigits(number));
+        System.out.println("(Recursive) The sum of digits in " + number + " is " + sumDigitsRecursive(number));
+    }
+
+    public static int sumDigits(long n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static int sumDigitsRecursive(long n) {
+        if (n==0)
+            return 0;
+        else
+            return sumDigitsRecursive(n/10) + (int)(n%10);
+    }
+}
